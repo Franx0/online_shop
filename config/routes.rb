5e7a2b5e8ce6013bacb560eff65df128a7bae10a
Franx0 <francisco.moya.martinez@gmail.com>
2021-06-17 18:45:21 +0200

    add api version and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
+require "./lib/api/version"
+
 Rails.application.routes.draw do
+  if !Rails.env.production?
+    # Swagger UI
+    mount Rswag::Ui::Engine => '/api-docs'
+    mount Rswag::Api::Engine => '/api-docs'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    scope module: :v1, constraints: ApiVersion.new('v1', true) do
+      concern :with_disbursements do
+        member do
+          get :disbursements, to: 'disbursements#show'
+        end
+
+        collection do
+          get :disbursements, to: 'disbursements#index'
+        end
+      end
+
+      resources :merchants, concerns: :with_disbursements, only: [:disbursements]
+    end
+  end
 end
