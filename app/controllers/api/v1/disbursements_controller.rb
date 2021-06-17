baa2e0c886a315237e494dbbcc09629d0287b479
Franx0 <francisco.moya.martinez@gmail.com>
2021-06-17 20:08:19 +0200

    add set_date method on controller to parse date

--- app/controllers/api/v1/disbursements_controller.rb
+++ app/controllers/api/v1/disbursements_controller.rb
@@ -3,15 +3,19 @@ module Api::V1
     before_action :set_resource, only: [:show]
 
     def index
-      render_json({ data: Merchant.new.disbursements }, 200)
+      render_json({ data: Merchant.new.disbursements(date: set_date) }, 200)
     end
 
     def show
       date = params[:date]&.to_datetime&.beginning_of_week
-      render_json({ data: @resource.disbursements(date: date) }, 200)
+      render_json({ data: @resource.disbursements(date: set_date) }, 200)
     end
 
     private
+
+    def set_date
+      params[:date]&.to_datetime&.beginning_of_week
+    end
 
     def set_resource
       @resource = (request.path.split('/').slice 2)&.classify&.constantize.find(params[:id])
